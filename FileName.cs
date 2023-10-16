baed934fd7ccd34a34008052725ff025f9dcc008
user <user@WIN-F7NIMF7A3VO>
2023-10-16 18:41:56 +0500

    Добавьте файлы проекта.

---
+++ FileName.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using TG_Quitz;
+
+
+public static class TestRepository
+{
+    public static List<Test> GetTests()
+    {
+        var stringTest = File.ReadAllText("D:\\Study\\.Net(Najot)\\C#\\TG_Quitz\\Test.json");
+        var tests = JsonSerializer.Deserialize<List<Test>>(stringTest);
+
+        return tests;
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,12 @@
+﻿namespace TG_Quitz
+{
+    public class Test
+    {
+        public string Question { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string CorrectAnswer { get; set; }
+    }
+}
